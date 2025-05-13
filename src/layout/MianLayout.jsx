fbc9e76d1fe3eb2451cedd76af5a97f55870d502
MaryamZaghlool <maryamzaghlool0@gmail.com>
2025-05-13 22:07:07 +0300

    done

--- src/layout/MianLayout.jsx
+++ src/layout/MianLayout.jsx
@@ -12,6 +12,10 @@ import MovieDetails from "../pages/MovieDetails";
 import Movies from "../pages/Movies";
 import SharedLayout from "./SharedLayout";
 import AdminLayout from "./AdminLayout";
+import ActorsPage from "../pages/ActorsPage";
+
+// import RegisterForm from "../components/Form/RegisterForm/RegisterForm";
+// import LoginForm from "../components/Form/LoginForm/LoginForm";
 
 export default function MianLayout() {
   return (
@@ -26,9 +30,10 @@ export default function MianLayout() {
           <Route path="movie/:id" element={<MovieDetails />} />
         </Route>
 
-        <Route path="/admin" element={<AdminLayout/>}>
-          <Route path="movie/:id" element={<MovieDetails />} />
-          <Route path="dashboard" element={<AdminView />} />
+
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" index element={<AdminView />} />
           <Route path="dashboard/all" element={<ViewAll />} />
           <Route path="Actors" element={<ActorsPage />} />
           {/* <Route path="movie/:id" element={<MovieForm />} /> */}
